3cc9e746babfcaa9127fb515dc81bfe9cb9f6ffc
KekmaTime <22am014@sctce.ac.in>
2024-01-26 11:56:03 +0530

    feat: implement TUI-based password generator Implement a terminal-based password generator with a TUI , offering an interactive experience for generating and displaying random passwords.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::event::{read, Event, KeyCode};
+use crossterm::terminal::{self, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::ExecutableCommand;
+use rand::distributions::Alphanumeric;
+use rand::Rng;
+use ratatui::backend::CrosstermBackend;
+use ratatui::layout::{Constraint, Direction, Layout};
+use ratatui::style::{Color, Style};
+use ratatui::widgets::{Block, Borders, Paragraph};
+use ratatui::Terminal;
+use std::io::{stdout, Result};
+
+fn main() -> Result<()> {
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut term = Terminal::new(CrosstermBackend::new(stdout()))?;
+    term.clear()?;
+
+    let mut pass: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .map(char::from)
+        .collect();
+
+    loop {
+        term.draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints(
+                    [
+                        Constraint::Percentage(10),
+                        Constraint::Percentage(80),
+                        Constraint::Percentage(10),
+                    ]
+                    .as_ref(),
+                )
+                .split(f.size());
+
+            let block = Block::default()
+                .title("Testing")
+                .style(Style::default().fg(Color::Green).bg(Color::Black))
+                .borders(Borders::ALL);
+            f.render_widget(block, chunks[0]);
+
+            let paragraph = Paragraph::new(&*pass)
+                .style(Style::default().fg(Color::Green).bg(Color::Black))
+                .block(Block::default().borders(Borders::ALL));
+            f.render_widget(paragraph, chunks[1]);
+        })?;
+
+        if let Event::Key(event) = read()? {
+            match event.code {
+                KeyCode::Char('q') => {
+                    break;
+                }
+                _ => {
+                    pass = rand::thread_rng()
+                        .sample_iter(&Alphanumeric)
+                        .take(30)
+                        .map(char::from)
+                        .collect();
+                }
+            }
+        }
+    }
+
+    let _ = stdout().execute(LeaveAlternateScreen);
+    terminal::disable_raw_mode()?;
+    Ok(())
 }
